a68dffac831da8a6bff7658bb036bd4233c2c9fe
dtamez <zematynnad@gmail.com>
2026-02-17 17:09:05 -0600

    Create list of valid jotto words.

--- jotto/game/views.py
+++ jotto/game/views.py
@@ -1,4 +1,3 @@
-import ipdb
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.core.cache import cache
---
+++ make_jotto_words.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+WORDS_FILE_NAME = "words.txt"
+JOTTO_FILE_NAME = "jotto_words.txt"
+
+
+def is_valid_jotto_word(word: str):
+    if "'" in word:
+        return False
+    word = word.strip()
+    if len(word) == 5:
+        return True
+
+
+def main():
+    pass
+    # read through words.txt
+    # toss out words with apostrophes
+    # toss out words that are not 5 letters long
+    # write to jotto_words.txt
+    with open(WORDS_FILE_NAME, "r") as orig:
+        with open(
+            JOTTO_FILE_NAME,
+            "w",
+        ) as jotto:
+            for word in orig.readlines():
+                word = str.lower(word)
+                if is_valid_jotto_word(word):
+                    jotto.write(word)
+
+
+if __name__ == "__main__":
+    main()
